b63665dcbdd0c3a21af08a721ee4b032f7d3197e
djgrant <danieljohngrant@gmail.com>
2012-12-05 10:00:40 +0000

    Fix to render in Firefox

--- chosenImage/chosenImage.jquery.js
+++ chosenImage/chosenImage.jquery.js
@@ -1,51 +1,56 @@
 ;;(function($) {
 
-	$.fn.chosenImage = function(options) {
+    $.fn.chosenImage = function(options) {
 
-		return this.each(function() {
+        return this.each(function() {
 
-			var $select = $(this);
+            var $select = $(this),
+                imgMap  = {};
 
-			// 1. Retrieve img-src from data attribute and set as background image using style attr.
-			$select.find('option').each(function() {
-				if($(this).attr('data-img-src')) {
-					var imgSrc = $(this).attr('data-img-src');
-					$(this).css(cssObj(imgSrc));
-				}
-			});
-
-
-			// 2. Execute chosen plugin
-			$select.chosen(options);
+            // 1. Retrieve img-src from data attribute and build object of image sources for each list item
+            $select.find('option').each(function(i) {
+                    var imgSrc   = $(this).attr('data-img-src');
+                    imgMap[i]    = imgSrc;
+            });
 
 
-			// 3. Change chosen selected element when form changes
-			var  chzn	   = '#' + $select.attr('id') + '_chzn',
-				$chzn      = $(chzn).addClass('chznImage-container');
+            // 2. Execute chosen plugin
+            $select.chosen(options);
 
-			$select.change(function() {
-				var imgSrc = ($select.find('option:selected').attr('data-img-src')) 
-								? $select.find('option:selected').attr('data-img-src') : '';
-				$chzn.find('.chzn-single span').css(cssObj(imgSrc));
-			});
-
-			$select.trigger('change');
+            var  chzn      = '#' + $select.attr('id') + '_chzn',
+                $chzn      = $(chzn).addClass('chznImage-container');
 
 
-			// Utilties
-			function cssObj(imgSrc) {
-				if(imgSrc) {
-					return {
-						'background-image': 'url(' + imgSrc + ')',
-						'background-repeat': 'no-repeat'
-					}
-				} else {
-					return {
-						'background-image': 'none'
-					}
-				}
-			}
-		});
-	}
+            // 3. Style lis with image sources
+            $chzn.find('.chzn-results li').each(function(i) {
+                $(this).css(cssObj(imgMap[i]));
+            });
+
+
+            // 4. Change image on chosen selected element when form changes
+            $select.change(function() {
+                var imgSrc = ($select.find('option:selected').attr('data-img-src')) 
+                                ? $select.find('option:selected').attr('data-img-src') : '';
+                $chzn.find('.chzn-single span').css(cssObj(imgSrc));
+            });
+
+            $select.trigger('change');
+
+
+            // Utilties
+            function cssObj(imgSrc) {
+                if(imgSrc) {
+                    return {
+                        'background-image': 'url(' + imgSrc + ')',
+                        'background-repeat': 'no-repeat'
+                    }
+                } else {
+                    return {
+                        'background-image': 'none'
+                    }
+                }
+            }
+        });
+    }
 
 })(jQuery);
